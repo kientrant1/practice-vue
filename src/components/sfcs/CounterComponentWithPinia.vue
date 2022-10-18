a97314be26e84a3fd05574b408d3e6987c4c9639
Kien Tran <kien.trantrung@clubspark.com>
2022-10-18 10:58:17 +0700

    pinia

--- src/components/sfcs/CounterComponentWithPinia.vue
+++ src/components/sfcs/CounterComponentWithPinia.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { useCounterStore } from '@/store/counterStore'
+import { useCounterStore, useCounterOptionalStore } from '@/store/counterStore'
 const counterStore = useCounterStore()
-const { counterRef } = storeToRefs(counterStore)
+const counterOptionalStore = useCounterOptionalStore()
 
 const { counter } = counterStore
+const { counterRef } = storeToRefs(counterStore)
+
+const { counterOptional, nameOptional, doubleCountOptional } = storeToRefs(counterOptionalStore)
+const { incrementCounterOptional } = counterOptionalStore
+
+
 
 const increaseCounterRefHandler = () => {
   counterRef.value++ //=> .value is undefined if no using "storeToRefs"
@@ -17,5 +23,14 @@ const increaseCounterRefHandler = () => {
   <br/><br/>
   <div>count with non-primitive value (without wrapping with storeToRefs): {{ counter.count }}</div>
   <button @click="counter.count++">Increase Counter</button>
+  <br/><br/>
+  <div>
+    count (optional API): {{ counterOptional }} <br/>
+    Double count (optional API): {{ doubleCountOptional }} <br/>
+    Count name (optional API): {{ nameOptional }} <br/>
+  </div>
+  Change name directs in State (CAN via storeToRefs) <input v-model="nameOptional" type="text"/> <br/>
+  Change double count getter directs (CAN NOT) <input v-model="doubleCountOptional" placeholder="Double count" type="number"/> <br/>
+  Increase number in method (CAN) <button @click="incrementCounterOptional">Increase Counter</button>
 
 </template>
--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -8,13 +8,32 @@ const initialState: ICounter = {
   count: 0,
 } 
 
+// Composition API
 export const useCounterStore = defineStore('counterStore', () => {
   // state is object data
   const counter = reactive<ICounter>(initialState)
   const counterRef = ref<number>(0)
 
+  const incrementCounterRef = () => {
+    counterRef.value++
+  }
+
   return {
     counter,
-    counterRef
+    counterRef,
+    incrementCounterRef
   }
+})
+
+// Optional API
+export const useCounterOptionalStore = defineStore('counterOptionalStore', {
+  state: () => ({ counterOptional: 0, nameOptional: 'Eduardo' }),
+  getters: {
+    doubleCountOptional: (state) => state.counterOptional * 2,
+  },
+  actions: {
+    incrementCounterOptional() {
+      this.counterOptional++
+    },
+  },
 })
